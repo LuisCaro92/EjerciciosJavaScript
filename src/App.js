f073c5d5f01fdcb3ecc3c626c053f81732167190
luis caro <luis.carosalgado1@gmail.com>
2023-07-14 22:49:35 -0400

    comenzando

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@
 import './App.css';
 
 function App() {
+
+
+const FuncionSiglos =(numero) =>{
+let years = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
+const siglo = years.map(year => year) 
+if (siglo === numero)
+return ("siglo 20")
+
+}
+
+
+console.log(FuncionSiglos(1992))
+
   return (
     <div className="App">
      
