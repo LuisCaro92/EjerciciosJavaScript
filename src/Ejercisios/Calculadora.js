d4235f388456e39dde4bdb228eff4fc5aa5bdac8
luis caro <luis.carosalgado1@gmail.com>
2023-08-24 17:41:31 -0400

    calculadora casi terminada

--- src/Ejercisios/Calculadora.js
+++ src/Ejercisios/Calculadora.js
@@ -30,26 +30,24 @@ const Calculadora = () => {
 
   return (
     <div className="justify-center mx-auto bg-slate-500">
-      <h1 className=" text-5xl text-yellow-500 flex justify-center m-10">Calculadora</h1>
+      <h1 className=" text-5xl text-yellow-500 flex justify-center m-10">
+        Calculadora
+      </h1>
       <div className="flex justify-center bg-slate-300">
         <div className="bg-slate-500 w-xl">
-        <span className="text-3xl">{click}</span>
+          <span className="text-3xl">{click}</span>
         </div>
       </div>
       <div className="container-sm  flex justify-center items-center bg-slate-300">
-    
         <div className="grid grid-cols-4 items-center">
-          <div className="m-2">
-            <button className="w-20 h-20 rounded-xl bg-slate-600">a</button>
-          </div>
-          <div className="m-2">
-            <button className="w-20 h-20 rounded-xl bg-slate-600">b</button>
-          </div>
-          <div className="m-2">
-            <button className="w-20 h-20 rounded-xl bg-slate-600">c</button>
-          </div>
-          <div className="m-2">
-            <button className="w-20 h-20 rounded-xl bg-slate-600">d</button>
+          <div className="m-4">
+            {botones.map((boton) => (
+              <button className="w-20 h-20 rounded-xl bg-yellow-600 m-2 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110  duration-500 ease-in-out">
+                {boton.operador}
+                {boton.id}
+                {boton.igual}
+              </button>
+            ))}
           </div>
         </div>
       </div>
