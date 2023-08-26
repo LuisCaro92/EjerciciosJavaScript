3a0fb91a755b675f9da2d3a43815ff13fb1e1d55
luis caro <luis.carosalgado1@gmail.com>
2023-08-25 22:16:13 -0400

    casi ok la calculadora

--- src/Ejercisios/Calculadora.js
+++ src/Ejercisios/Calculadora.js
@@ -11,7 +11,7 @@ const Calculadora = () => {
     { operador: "C" },
     { operador: "/" },
     { operador: "X" },
-    { operador: "<=" },
+    { operador: "<-" },
     { id: 7 },
     { id: 8 },
     { id: 9 },
@@ -30,26 +30,27 @@ const Calculadora = () => {
   ];
 
   return (
-    <div className="justify-center mx-auto bg-slate-500">
+    <div className="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-slate-500  border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
       <h1 className=" text-5xl text-yellow-500 flex justify-center m-10">
         Calculadora
       </h1>
-      <div className="flex justify-center bg-slate-300">
-        <div className="bg-slate-500 w-xl">
-          <span className="text-3xl">{click}</span>
-        </div>
+
+      <div className="bg-blue-300 h- 20 rounded-sm">
+        <span className="flex justify-end text-7xl">{click}</span>
       </div>
-      <div className="container-sm  flex justify-center items-center bg-slate-300">
-        <div className="grid grid-cols-4 items-center">
-          <div className="m-4">
-            {botones.map((boton) => (
-              <button className="w-20 h-20 rounded-xl bg-yellow-600 m-2 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110 active:scale-50 duration-500 ease-in-out hover:bg-yellow-400">
+
+      <div className="flex items-stretch bg-slate-500 h-24">
+        <div className="flex-1 justify-center flex items-center">
+        {botones.map((boton) => (
+          <div className="m-1 flex items-center justify-center">
+              <button className="flex items-center justify-center w-20 h-20 rounded-xl bg-yellow-600 m-2 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110 active:scale-50 duration-500 ease-in-out hover:bg-yellow-400">
                 {boton.operador}
                 {boton.id}
                 {boton.igual}
               </button>
-            ))}
           </div>
+            ))}
+
         </div>
       </div>
     </div>
