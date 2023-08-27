abf5d3480ca2ddca182373fa9f9bbb1b4ef9aafa
luis caro <luis.carosalgado1@gmail.com>
2023-08-26 21:25:33 -0400

    ordenada la calculadora solo falta la funcionalidad

--- src/Ejercisios/Calculadora.js
+++ src/Ejercisios/Calculadora.js
@@ -5,28 +5,30 @@ const Calculadora = () => {
 
   const botones = [
     { operador: "MC" },
-    { operador: "M+" },
-    { operador: "M-" },
-    { operador: "MR" },
     { operador: "C" },
-    { operador: "/" },
-    { operador: "X" },
-    { operador: "<-" },
     { id: 7 },
-    { id: 8 },
-    { id: 9 },
-    { operador: "-" },
     { id: 4 },
-    { id: 5 },
-    { id: 6 },
-    { operador: "+" },
     { id: 1 },
-    { id: 2 },
-    { id: 3 },
-    { igual: "=" },
     { id: "%" },
+
+    { operador: "M+" },
+    { operador: "/" },
+    { id: 8 },
+    { id: 5 },
+    { id: 2 },
     { id: 0 },
+
+    { operador: "M-" },
+    { operador: "X" },
+    { id: 9 },
+    { id: 6 },
+    { id: 3 },
     { id: "," },
+
+    { operador: "MR" },
+    { operador: "<-" },
+    { operador: "-" },
+    { operador: "+" },
   ];
 
   return (
@@ -41,16 +43,17 @@ const Calculadora = () => {
 
       <div className="flex bg-slate-500 ">
         <div className="grid grid-rows-6 grid-flow-col mx-auto">
-        {botones.map((boton) => (
-          <div className="m-1">
+          {botones.map((boton) => (
+            <div className="m-1">
               <button className="text-3xl w-20 h-20 rounded-xl bg-yellow-600 m-1 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110 active:scale-50 duration-500 ease-in-out hover:bg-yellow-400">
                 {boton.operador}
                 {boton.id}
-                {boton.igual}
               </button>
-          </div>
-            ))}
-
+            </div>
+          ))}
+          <button className="text-3xl w-20 h-20 rounded-xl bg-yellow-800 m-1 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110 active:scale-50 duration-500 ease-in-out hover:bg-yellow-400">
+            =
+          </button>
         </div>
       </div>
     </div>
