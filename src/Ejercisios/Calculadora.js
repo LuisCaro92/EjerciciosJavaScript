6b8f02eab307df8b2c517d0bf63936398eec0d1c
luis caro <luis.carosalgado1@gmail.com>
2023-08-27 20:52:33 -0400

    agregando funcionalidad

--- src/Ejercisios/Calculadora.js
+++ src/Ejercisios/Calculadora.js
@@ -31,6 +31,9 @@ const Calculadora = () => {
     { operador: "+" },
   ];
 
+  const botonSuma =()=>{
+    return setClick(click + botones.id)
+  }
   return (
     <div className="mx-auto overflow-hidden mt-10 shadow-lg mb-2 bg-slate-500  border rounded-lg lg:w-2/6 md:w-3/6 sm:w-4/6">
       <h1 className=" text-5xl text-yellow-500 flex justify-center m-10">
@@ -50,6 +53,7 @@ const Calculadora = () => {
                 {boton.id}
               </button>
             </div>
+            
           ))}
           <button className="text-3xl w-20 h-20 rounded-xl bg-yellow-800 m-1 shadow-[0px_10px_1px_rgba(221,_221,_221,_1),_0_10px_20px_rgba(204,_204,_204,_1)] hover:scale-110 active:scale-50 duration-500 ease-in-out hover:bg-yellow-400">
             =
