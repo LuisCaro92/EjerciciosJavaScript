6beb856a6c1fe0ecd237a8c83d337bdbbe9381f9
luis caro <luis.carosalgado1@gmail.com>
2023-08-29 18:28:31 -0400

    mejorado el nav

--- src/Ejercisios/CarritoCompras.js
+++ src/Ejercisios/CarritoCompras.js
@@ -29,7 +29,7 @@ const CarritoCompras = () => {
           Tienda Ramdon
         </h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
+        <div className="mt-6 grid grid-cols-1 gap-x-10 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 ">
           {products.map((product) => (
             <div className="group relative bg-slate-400 rounded-md hover:shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset]">
               <div className="flex items-center mx-auto aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
@@ -59,8 +59,8 @@ const CarritoCompras = () => {
                 </p>
               </div>
               <div className="flex justify-center my-10 ">
-                <button className="group [transform:translateZ(0)] px-6 py-3 w-40 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-green-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500">
-                  <span className="text-xl relative z-0 text-black group-hover:text-gray-200 transition ease-in-out duration-500">
+                <button className="group [transform:translateZ(0)] px-6 py-3 max-h-15 w-40 rounded-lg bg-gray-200 overflow-hidden relative before:absolute before:bg-green-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500">
+                  <span className="text-2xl relative z-0  group-hover:text-gray-200 transition ease-in-out duration-500">
                   {carrito}
                   </span>
                 </button>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,21 +9,20 @@ const Nav = () => {
     const carro = <FontAwesomeIcon icon={faCartPlus} />;
 
   return (
-    <div className=" bg-slate-200 w-full h-12 text-center">
-      <ul className="flex  justify-center">
+    <div className=" bg-slate-200 w-full h-15 text-center">
+      <ul className="flex justify-end">
         <Link to="/Calculadora">
-          <li className="p-2 mx-10 text-black ">Calculadora</li>
+          <li className="p-2 ml-20 text-2xl font-semibold text-black cursor-pointer hover:text-fuchsia-400">Calculadora</li>
         </Link>
 
         <Link to="/Contador">
-          <li className="p-2 mx-10 text-black ">Contador</li>
+          <li className="p-2 ml-20 text-2xl font-semibold text-black cursor-pointer hover:text-fuchsia-400">Contador</li>
         </Link>
 
         <Link to="/Carrito">
-          <li className="flex justify-end p-2  text-black ">{carro}</li>
+          <li className="mt-1 ml-20 mr-5 text-4xl text-green-400 cursor-pointer hover:scale-110 active:scale-90 transition-transform ease-in-out duration-200">{carro}</li>
         </Link>
 
-        <li className="p-2 mx-10 text-black "></li>
       </ul>
     </div>
   );
